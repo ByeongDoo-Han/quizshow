c139e350a5080dcff22ee5bccdb1f186677f161a
DESKTOP-075F8IT\hanbd <bdhan@scatterx.io>
2023-12-13 19:21:03 +0900

    quiz controller 추가

---
+++ src/main/java/com/example/quizshow/controller/QuizController.java
@@ -0,0 +1,21 @@
+package com.example.quizshow.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+public class QuizController {
+
+    @Value("${secret.code.speed1}")
+    private String speed1;
+    @PostMapping("/speedquiz1")
+    public ModelAndView getSpeedQuiz1(@RequestBody String secretCode){
+        ModelAndView mv = new ModelAndView("/resources/static/speed1.html");
+        if(secretCode.equals(speed1)) return mv;
+        else return null;
+    }
+}
